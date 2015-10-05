06d9afbbe53fac8910f8f970ddc02c817bd56508
konceptsandcode <koncepts.and.code@gmail.com>
2015-10-05 13:24:07 -0400

    storage concepts in regards to strings

---
+++ Strings/StringCreation/src/StringStorage.java
@@ -0,0 +1,30 @@
+/**
+ * Created by droid on 05/10/15.
+ */
+public class StringStorage {
+    public static void main(String[] args) {
+        /*
+            a new String object is created in the non-pool area
+            a new String literal object is created in the String Constant Pool area
+
+            Therefore, the following line creates two objects
+         */
+        String s1 = new String("Hello World!");
+
+        /*
+            a new String object is created in the String Constant Pool area
+
+            Therefore, the following line creates only one object
+         */
+        String s2 = "Hi There!";
+
+        /*
+            a new String object is created in the non-pool area
+            Since an object with the literal "Hello World!" already exists on the String Constant Pool area, its
+            reference is returned, instead of creating a new object in the String Constant Pool area
+
+            Therefore, the following line creates only one object
+         */
+        String s3 = new String("Hello World!");
+    }
+}
